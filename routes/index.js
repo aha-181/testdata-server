24e66662ef31de0536ca21179b494e60af1d62d8
aha93 <a1hauser@hsr.ch>
2017-04-21 02:20:41 +0200

    Gerüst für das Auslösen des Testings vorbereitet

--- routes/index.js
+++ routes/index.js
@@ -17,11 +17,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Testdata-Server' });
 });
 
-//TODO: ACCURACY-TESTING
-//TODO: Call HSR-Server
-//TODO: Compare Results with MySQL-Tagging
-//TODO: Output Result
 
+// manuelles Tagging:
 router.get('/getMobileData', function(req, res, next) {
 
     var query = 'SELECT * FROM mobile_data_log as v1 INNER JOIN (SELECT MeasurementID FROM mobile_data_log WHERE ExpectedGroundID IS NULL LIMIT 1) as v2 ON v1.MeasurementID = v2.MeasurementID;';
@@ -36,10 +33,7 @@ router.get('/getMobileData', function(req, res, next) {
     });
 });
 
-
 router.post('/tagMeasurement', function (req, res) {
-
-    //var query = 'UPDATE mobile_data_log SET Tag = \'unknown\' WHERE MeasurementID = 37457283';
 
     var sql = 'UPDATE mobile_data_log SET ExpectedGroundID = ?, ExpectedSurroundingID = ?, ExpectedTypeOfMotionID = ?, ExpectedPopulationDensityID = ? WHERE MeasurementID = ?';
     var inserts = [req.body.groundTag, req.body.surroundingTag, req.body.typeOfMotionTag, req.body.populationDensityTag, req.body.measurementId];
@@ -50,13 +44,27 @@ router.post('/tagMeasurement', function (req, res) {
     connection.query(sql, function (error, results, fields) {
 
         if(error) {
-            res.render('result', {result: 'error'});
+            res.render('manualTaggingFeedback', {result: 'error'});
             throw error;
         } else {
-            res.render('result', {result: 'success'});
+            res.render('manualTaggingFeedback', {result: 'success'});
         }
     });
 });
 
+
+//TODO: ACCURACY-TESTING
+//TODO: Call HSR-Server
+//TODO: Compare Results with MySQL-Tagging
+//TODO: Output Result
+
+// Testen der Messungen
+router.get('/testMeasurements', function(req, res, next) {
+    res.render('testingIndex');
+});
+
+router.post('/startTest', function(req, res) {
+    console.log(req.body.version);
+});
 
 module.exports = router;
