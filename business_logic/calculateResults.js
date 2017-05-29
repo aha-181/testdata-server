c90d4fd9dcaa865ba7d12be02c0e3938bc54b12c
aha93 <a1hauser@hsr.ch>
2017-05-29 20:42:49 +0200

    Auswertungen erweitert

--- business_logic/calculateResults.js
+++ business_logic/calculateResults.js
@@ -51,56 +51,82 @@ function getTestResults(testNumber, res) {
 }
 
 function renderCalculation(res, results) {
-    var locationTotal = 0;
+    var locationTotalCorrect = 0;
 
     for(var i = 0; i < results.length; i++) {
-        locationTotal += results[i].ReturnedLocationIsCorrect;
+        locationTotalCorrect += results[i].ReturnedLocationIsCorrect;
     }
 
-    var locationPercent = Math.round((locationTotal / results.length) * 100);
+    var shareLocationCorrect = Math.round((locationTotalCorrect / results.length) * 100);
     var testID = results[0].TestMeasurementID;
 
-    var versionQuery = 'SELECT Version FROM resulting_correctness WHERE TestMeasurementID = ?';
+    var versionQuery = 'SELECT Version FROM resulting_correctness WHERE TestMeasurementID = ?;';
+    var totalUnknown = 'SELECT count(*) as count FROM returned_measurement_values WHERE ReturnedLocationID = -1 AND TestMeasurementID = ?;';
+    var totalRejected = 'SELECT count(*) as count FROM returned_measurement_values WHERE ReturnedLocationID >= 100 AND TestMeasurementID = ?;';
+
     var railwayCorrectQuery = 'SELECT count(*) as count FROM returned_measurement_values WHERE ExpectedLocationID = 1 AND ' +
         'ReturnedLocationIsCorrect = 1 AND TestMeasurementID = ?;';
-    var railwayWrongQuery = 'SELECT count(*) as count FROM returned_measurement_values WHERE ExpectedLocationID = 1 AND ' +
-        'ReturnedLocationIsCorrect = 0 AND TestMeasurementID = ?;';
-    var railwayUnwnownQuery = 'SELECT count(*) as count FROM returned_measurement_values WHERE ExpectedLocationID = 1 AND ' +
+    var railwayTotalQuery = 'SELECT count(*) as count FROM returned_measurement_values WHERE ExpectedLocationID = 1 AND ' +
+        'TestMeasurementID = ?;';
+    var railwayUnknownQuery = 'SELECT count(*) as count FROM returned_measurement_values WHERE ExpectedLocationID = 1 AND ' +
         'ReturnedLocationID = -1 AND TestMeasurementID = ?;';
+    var railwayRejectedQuery = 'SELECT count(*) as count FROM returned_measurement_values WHERE ExpectedLocationID = 1 AND ' +
+        'ReturnedLocationID >= 100 AND TestMeasurementID = ?;';
+
     var streetCorrectQuery = 'SELECT count(*) as count FROM returned_measurement_values WHERE ExpectedLocationID = 2 AND ' +
         'ReturnedLocationIsCorrect = 1 AND TestMeasurementID = ?;';
-    var streetWrongQuery = 'SELECT count(*) as count FROM returned_measurement_values WHERE ExpectedLocationID = 2 AND ' +
-        'ReturnedLocationIsCorrect = 0 AND TestMeasurementID = ?;';
+    var streetTotalQuery = 'SELECT count(*) as count FROM returned_measurement_values WHERE ExpectedLocationID = 2 AND ' +
+        'TestMeasurementID = ?;';
     var streetUnknownQuery = 'SELECT count(*) as count FROM returned_measurement_values WHERE ExpectedLocationID = 2 AND ' +
         'ReturnedLocationID = -1 AND TestMeasurementID = ?;';
+    var streetRejectedQuery = 'SELECT count(*) as count FROM returned_measurement_values WHERE ExpectedLocationID = 2 ' +
+        'AND ReturnedLocationID >= 100 AND TestMeasurementID = ?;';
+
     var buildingCorrectQuery = 'SELECT count(*) as count FROM returned_measurement_values WHERE ExpectedLocationID = 3 AND ' +
         'ReturnedLocationIsCorrect = 1 AND TestMeasurementID = ?;';
-    var buildingWrongQuery = 'SELECT count(*) as count FROM returned_measurement_values WHERE ExpectedLocationID = 3 AND ' +
-        'ReturnedLocationIsCorrect = 0 AND TestMeasurementID = ?;';
+    var buildingTotalQuery = 'SELECT count(*) as count FROM returned_measurement_values WHERE ExpectedLocationID = 3 AND ' +
+        'TestMeasurementID = ?;';
     var buildingUnknownQuery = 'SELECT count(*) as count FROM returned_measurement_values WHERE ExpectedLocationID = 3 AND ' +
         'ReturnedLocationID = -1 AND TestMeasurementID = ?;';
-    var totalError200 = 'SELECT count(*) as count FROM returned_measurement_values WHERE ReturnedLocationID = 100 AND TestMeasurementID = ?;';
-    var railwayError200 = 'SELECT count(*) as count FROM returned_measurement_values WHERE ExpectedLocationID = 1 AND ' +
-        'ReturnedLocationID = 100 AND TestMeasurementID = ?;';
+    var buildingRejectedQuery = 'SELECT count(*) as count FROM returned_measurement_values WHERE ExpectedLocationID = 3 AND ' +
+        'ReturnedLocationID >= 100 AND TestMeasurementID = ?;';
+
+    var totalErrorAccuracy = 'SELECT count(*) as count FROM returned_measurement_values WHERE ReturnedLocationID = 100 AND TestMeasurementID = ?;';
+    var totalErrorSameTime = 'SELECT count(*) as count FROM returned_measurement_values WHERE ReturnedLocationID = 101 AND TestMeasurementID = ?;';
+    var totalErrorTooFar = 'SELECT count(*) as count FROM returned_measurement_values WHERE ReturnedLocationID = 102 AND TestMeasurementID = ?;';
+    var totalErrorLonLat = 'SELECT count(*) as count FROM returned_measurement_values WHERE ReturnedLocationID = 103 AND TestMeasurementID = ?;';
+
     var inserts = [testID];
     versionQuery = mysql.format(versionQuery, inserts);
+    totalUnknown = mysql.format(totalUnknown, inserts);
+    totalRejected = mysql.format(totalRejected, inserts);
+
     railwayCorrectQuery = mysql.format(railwayCorrectQuery, inserts);
-    railwayWrongQuery = mysql.format(railwayWrongQuery, inserts);
-    railwayUnwnownQuery = mysql.format(railwayUnwnownQuery, inserts);
+    railwayTotalQuery = mysql.format(railwayTotalQuery, inserts);
+    railwayUnknownQuery = mysql.format(railwayUnknownQuery, inserts);
+    railwayRejectedQuery = mysql.format(railwayRejectedQuery, inserts);
+
     streetCorrectQuery = mysql.format(streetCorrectQuery, inserts);
-    streetWrongQuery = mysql.format(streetWrongQuery, inserts);
+    streetTotalQuery = mysql.format(streetTotalQuery, inserts);
     streetUnknownQuery = mysql.format(streetUnknownQuery, inserts);
+    streetRejectedQuery = mysql.format(streetRejectedQuery, inserts);
+
     buildingCorrectQuery = mysql.format(buildingCorrectQuery, inserts);
-    buildingWrongQuery = mysql.format(buildingWrongQuery, inserts);
+    buildingTotalQuery = mysql.format(buildingTotalQuery, inserts);
     buildingUnknownQuery = mysql.format(buildingUnknownQuery, inserts);
-    totalError200 = mysql.format(totalError200, inserts);
-    railwayError200 = mysql.format(railwayError200, inserts);
+    buildingRejectedQuery = mysql.format(buildingRejectedQuery, inserts);
+
+    totalErrorAccuracy = mysql.format(totalErrorAccuracy, inserts);
+    totalErrorSameTime = mysql.format(totalErrorSameTime, inserts);
+    totalErrorTooFar = mysql.format(totalErrorTooFar, inserts);
+    totalErrorLonLat = mysql.format(totalErrorLonLat, inserts);
 
     parallel([
             function(callback) {
-                dbConnection.queryMultiple([versionQuery, railwayCorrectQuery, railwayWrongQuery, railwayUnwnownQuery, streetCorrectQuery,
-                    streetWrongQuery, streetUnknownQuery, buildingCorrectQuery, buildingWrongQuery, buildingUnknownQuery,
-                    totalError200, railwayError200], function(err, results) {
+                dbConnection.queryMultiple([versionQuery, railwayCorrectQuery, railwayTotalQuery, railwayUnknownQuery, streetCorrectQuery,
+                    streetTotalQuery, streetUnknownQuery, buildingCorrectQuery, buildingTotalQuery, buildingUnknownQuery,
+                    totalUnknown, totalRejected, totalErrorAccuracy, totalErrorSameTime, totalErrorTooFar, totalErrorLonLat,
+                    railwayRejectedQuery, streetRejectedQuery, buildingRejectedQuery], function(err, results) {
                     callback(err, results);
                 });
             }
@@ -111,37 +137,63 @@ function renderCalculation(res, results) {
                 var version = results[0][0][0].Version;
 
                 var railwayCorrect = results[0][1][0].count;
-                var railwayWrong = results[0][2][0].count;
+                var railwayTotal = results[0][2][0].count;
                 var railwayUnknown = results[0][3][0].count;
-                var railwayTotal = railwayCorrect + railwayWrong;
-                var railwayCorrectness = Math.round(railwayCorrect * 100 / railwayTotal);
-                var railwayShareUnknown = Math.round(railwayUnknown * 100 / railwayWrong);
+                var railwayRejected = results[0][16][0].count;
+                var railwayWrong = railwayTotal - railwayCorrect - railwayUnknown - railwayRejected;
+                var railwayCorrectness = Math.round(railwayCorrect * 100 / (railwayTotal - railwayRejected));
+                var railwayShareWrong = Math.round(railwayWrong * 100 / (railwayTotal - railwayRejected));
+                var railwayShareUnknown = Math.round(railwayUnknown * 100 / (railwayTotal - railwayRejected));
+                var railwayShareRejected = Math.round(railwayRejected * 100 / railwayTotal);
 
                 var streetCorrect = results[0][4][0].count;
-                var streetWrong = results[0][5][0].count;
+                var streetTotal = results[0][5][0].count;
                 var streetUnknown = results[0][6][0].count;
-                var streetTotal = streetCorrect + streetWrong;
-                var streetCorrectness = Math.round(streetCorrect * 100 / streetTotal);
-                var streetShareUnknown = Math.round(streetUnknown * 100 / streetWrong);
+                var streetRejected = results[0][17][0].count;
+                var streetWrong = streetTotal - streetCorrect - streetUnknown - streetRejected;
+                var streetCorrectness = Math.round(streetCorrect * 100 / (streetTotal - streetRejected));
+                var streetShareWrong = Math.round(streetWrong * 100 / (streetTotal - streetRejected));
+                var streetShareUnknown = Math.round(streetUnknown * 100 / (streetTotal - streetRejected));
+                var streetShareRejected = Math.round(streetRejected * 100 / streetTotal);
 
                 var buildingCorrect = results[0][7][0].count;
-                var buildingWrong = results[0][8][0].count;
+                var buildingTotal = results[0][8][0].count;
                 var buildingUnknown = results[0][9][0].count;
-                var buildingTotal = buildingCorrect + buildingWrong;
-                var buildingCorrectness = Math.round(buildingCorrect * 100 / buildingTotal);
-                var buildingShareUnknown = Math.round(buildingUnknown * 100 / buildingWrong);
+                var buildingRejected = results[0][18][0].count;
+                var buildingWrong = buildingTotal - buildingCorrect - buildingUnknown - buildingRejected;
+                var buildingCorrectness = Math.round(buildingCorrect * 100 / (buildingTotal - buildingRejected));
+                var buildingShareWrong = Math.round(buildingWrong * 100 / (buildingTotal - buildingRejected));
+                var buildingShareUnknown = Math.round(buildingUnknown * 100 / (buildingTotal - buildingRejected));
+                var buildingShareRejected = Math.round(buildingRejected * 100 / buildingTotal);
 
                 var totalMeasurements = railwayTotal + streetTotal + buildingTotal;
-                var totalUnknown = railwayUnknown + streetUnknown + buildingUnknown;
+                var totalUnknown = results[0][10][0].count;
                 var totalShareUnknown = Math.round(totalUnknown * 100 / totalMeasurements);
+                var totalRejected = results[0][11][0].count;
+                var totalShareRejected = Math.round(totalRejected * 100 / totalMeasurements);
+                var totalShareWrong = 100 - totalShareUnknown - totalShareRejected - shareLocationCorrect;
 
-                var totalError200 = Math.round(results[0][10][0].count * 100 / totalMeasurements);
-                var railwayError200 = Math.round(results[0][11][0].count * 100 / railwayTotal);
+                var totalErrorAccuracy = results[0][12][0].count;
+                var shareAccuracy = Math.round(totalErrorAccuracy * 100 / totalRejected);
+                var totalErrorSameTime = results[0][13][0].count;
+                var shareSameTime = Math.round(totalErrorSameTime * 100 / totalRejected);
+                var totalErrorTooFar = results[0][14][0].count;
+                var shareTooFar = Math.round(totalErrorTooFar * 100 / totalRejected);
+                var totalErrorLonLat = results[0][15][0].count;
+                var shareLonLat = Math.round(totalErrorLonLat * 100 / totalRejected);
 
-                res.render('resultOfTests', { testNumber: testID, version: version, location: locationPercent,
-                    railwayCorrect: railwayCorrectness, railwayUnknown: railwayShareUnknown, streetCorrect: streetCorrectness,
-                    streetUnknown: streetShareUnknown, buildingCorrect: buildingCorrectness, buildingUnknown: buildingShareUnknown,
-                    totalUnknown: totalShareUnknown, totalError200: totalError200, railwayError200: railwayError200});
+                var totalAccepted = totalMeasurements - totalRejected;
+                var shareAcceptedCorrect = Math.round(locationTotalCorrect * 100 / totalAccepted);
+                var shareAcceptedUnknown = Math.round(totalUnknown * 100 / totalAccepted);
+                var shareAcceptedWrong = 100 - shareAcceptedCorrect - shareAcceptedUnknown;
+
+                res.render('resultOfTests', { testNumber: testID, version: version,
+                    totalCount: totalMeasurements, totalCorrect: shareLocationCorrect, totalWrong: totalShareWrong, totalUnknown: totalShareUnknown, totalRejected: totalShareRejected,
+                    rejectedCount: totalRejected, rejectedAccuracy: shareAccuracy, rejectedSameTime: shareSameTime, rejectedTooFar: shareTooFar, rejectedLonLat: shareLonLat,
+                    acceptedCount: totalAccepted, acceptedCorrect: shareAcceptedCorrect, acceptedWrong: shareAcceptedWrong, acceptedUnknown: shareAcceptedUnknown,
+                    railwayCount: railwayTotal, railwayCorrect: railwayCorrectness, railwayWrong: railwayShareWrong, railwayUnknown: railwayShareUnknown, railwayRejected: railwayShareRejected,
+                    streetCount: streetTotal, streetCorrect: streetCorrectness, streetWrong: streetShareWrong, streetUnknown: streetShareUnknown, streetRejected: streetShareRejected,
+                    buildingCount: buildingTotal, buildingCorrect: buildingCorrectness, buildingWrong: buildingShareWrong, buildingUnknown: buildingShareUnknown, buildingRejected: buildingShareRejected });
             } else {
                 res.render('resultOfTests', {error: 'An error occurred'});
             }
